f8203c1bb2a967cbe87f3424ede7c008f7996977
tokyofool <96683259+tokyofool@users.noreply.github.com>
2021-12-26 05:41:21 -0500

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -9,7 +9,7 @@ import sys
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
+    parser.add_argument("-u", "250_.b3", type = str, default = "is underage smoking weed", help = "250_.b3")
     parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
@@ -22,8 +22,8 @@ args = getOptions()
 username = args.username
 acc_file = args.file
 
-if username == "" :
-	username = input("Username: ")
+if username == "250_.b3" :
+	username = input("250_.b3: ")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
